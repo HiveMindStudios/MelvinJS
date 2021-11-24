2159402406424fb10e5e2418b35d4afacb3a82b6
Michał Czyż <michal.czyz35@gmail.com>
2021-11-24 19:12:23 +0100

    Switched message to messageCreate

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ client.once('ready', () => {
   });
 });
 
-client.on("message", async message => {
+client.on("messageCreate", async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   const args = message.content.trim().split(/ +/g);
